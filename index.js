02e3c616b781a318a420c0769bb14ad185d6b959
jeffwhelpley <jeffwhelpley@hotmail.com>
2019-11-21 08:09:43 -0500

    Adding different functions to code

--- index.js
+++ index.js
@@ -1,23 +1,68 @@
-// Import stylesheets
 import './style.css';
 
-// here we add our function to the click event
-// on our button
+// get a reference to different parts of the page
 var mybutton = document.querySelector('.mybutton');
-mybutton.addEventListener('click', sayHi);
+var questionBox = document.querySelector('.questionBox');
+var answerBox = document.querySelector('.answerBox');
 
-// this our function that will
-// run when we click the button
-function sayHi() {
-  var randomColor = getRandomColor();
-  alert('Changing header to color ' + randomColor);
-  
-  var header = document.querySelector('.header');
-  header.style.color = randomColor;
+// assign a function handler to the click event
+mybutton.addEventListener('click', findGif);
+
+// this function will change the button color to a random color
+function changeButtonColor() {
+  mybutton.style.backgroundColor = getRandomColor();
 }
 
-// this is another function we will use to
-// generate a random color
+// add two numbers together
+function doMathProblem() {
+  var firstNumber = askForNumber('Please enter a number:');
+  var secondNumber = askForNumber('Now enter a second number:');
+  var answer = firstNumber + secondNumber;
+  questionBox.innerHTML = firstNumber + ' + ' + secondNumber;
+  answerBox.innerHTML = answer;
+  answerBox.style.display = 'block';
+}
+
+function findGif() {
+  var searchTerm = prompt('Enter search term:');
+  var giphyUrl = getGiphyUrl(searchTerm);
+
+  questionBox.innerHTML = 'Search term: ' + searchTerm;
+  answerBox.style.display = 'none'; 
+
+  fetch(giphyUrl)
+    .then(resp => resp.json())
+    .then(handleGif);
+}
+
+function handleGif(resp) {
+  console.log(resp.data[0].images.original.url);
+  answerBox.innerHTML = `<img src="${resp.data[0].images.original.url}">`;
+  answerBox.style.display = 'block';
+}
+
+// **************************************************
+// ************* HELPER FUNCTIONS *******************
+// **************************************************
+
+// helper function to get a giphy url
+function getGiphyUrl(searchTerm) {
+  return 'https://api.giphy.com/v1/gifs/search?api_key=w8BRwWuLdamTXtr2kydfXsfitj2WuwCm&limit=1&offset=0&rating=G&lang=en&q=' + searchTerm;
+}
+
+// helper function to get a number
+function askForNumber(message) {
+  var val = prompt(message);
+
+  try {
+    return parseInt(val);
+  } catch (ex) {
+    alert(val + ' is not a number!');
+    return getNumber(message);
+  }
+}
+
+// helper function to generate a random color
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
